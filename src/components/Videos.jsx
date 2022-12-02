877e9eea0aac20d7bc7f077763a471ab13b199c3
rumon5h <mdrumonkhan48519@gmail.com>
2022-12-02 20:40:22 +0600

    Video player and suggested videos section implementation done

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,15 +1,14 @@
 import {Box, Stack} from '@mui/material';
 import {VideoCard, ChannelCard} from './';
 
-const Videos = ({videos}) => {
+const Videos = ({videos, direction}) => {
     
     return (
-        <Stack direction='row' flexWrap='wrap' justifyContent='start' gap={2}>
-            {videos.map((item, index) => (
+        <Stack direction={direction || 'row'} flexWrap='wrap' justifyContent='start' gap={2}>
+            {videos?.map((item, index) => (
                 <Box key={index}>
                     {item.id.videoId && <VideoCard video={item}/>}
                     {item.id.channelId && <ChannelCard channelDetails={item}/>}
-
                 </Box>
             ))}
             
