d90f013c076c28676f1ed62e7c2d8aceccaefe92
rumon5h <mdrumonkhan48519@gmail.com>
2022-12-02 11:04:17 +0600

    Updated category name by category added

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,19 +1,26 @@
 import {useState, useEffect} from 'react';
-import {Stack, Box, Typography} from '@mui/material';
+import {Stack, Box, Typography, selectClasses} from '@mui/material';
 import {SideBar,Videos} from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const Feed = () => {
+    const [selectedCategory, setSelectedCategory] = useState('New')
+    useEffect(() => {
+        fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+    },[selectedCategory])
     return (
         <Stack sx={{flexDirection: {sx: 'column', md: 'row'}}}>
             <Box sx={{height: {sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px:{sx:0, md: 2}}}>
-            <SideBar/>
+
+            <SideBar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+
             <Typography className='copyRight' variant='body2' sx={{mt: 1.5, color: '#fff'}}>
             CopyRight 2022 Rumon 
             </Typography>
             </Box>
             <Box p={2} sx={{overFlowY: 'auto', height: '90vh', flex: 2}}>
                 <Typography variant='h4' fontWeight="bold" mb={2} sx={{color: 'white'}}>
-                    New <span style={{color: '#F31503'}}>videos</span>
+                    {selectedCategory} <span style={{color: '#F31503'}}>videos</span>
                 </Typography>
                 <Videos Videos={[]}/>
             </Box>
