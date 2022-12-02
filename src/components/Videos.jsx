be31bfe6fd069ac39c4d1a2b1f1a26eb7312a8fe
rumon5h <mdrumonkhan48519@gmail.com>
2022-12-02 15:36:50 +0600

    Channel card added

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -2,7 +2,7 @@ import {Box, Stack} from '@mui/material';
 import {VideoCard, ChannelCard} from './';
 
 const Videos = ({videos}) => {
-    console.log(videos)
+    
     return (
         <Stack direction='row' flexWrap='wrap' justifyContent='start' gap={2}>
             {videos.map((item, index) => (
