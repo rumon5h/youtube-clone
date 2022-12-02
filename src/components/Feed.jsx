8242280f44f88285df741cb23c1294e878aff283
rumon5h <mdrumonkhan48519@gmail.com>
2022-12-02 09:18:26 +0600

    Sidebar category added

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,14 +1,20 @@
 import {useState, useEffect} from 'react';
 import {Stack, Box, Typography} from '@mui/material';
+import SideBar from './SideBar';
 
 const Feed = () => {
     return (
         <Stack sx={{flexDirection: {sx: 'column', md: 'row'}}}>
             <Box sx={{height: {sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px:{sx:0, md: 2}}}>
-            Sidebar
+            <SideBar/>
             <Typography className='copyRight' variant='body2' sx={{mt: 1.5, color: '#fff'}}>
             CopyRight 2022 Rumon 
             </Typography>
+            </Box>
+            <Box>
+                <Typography variant='h4' fontWeight="bold" mb={2} sx={{color: 'white'}}>
+                    New <span style={{color: '#F31503'}}>videos</span>
+                </Typography>
             </Box>
         </Stack>
     );
--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,5 @@ export {default as Navbar} from './Navbar';
 export {default as SearchFeed} from './SearchFeed';
 export {default as VideoDetails} from './VideoDetails';
 export {default as ChannelDetails} from './ChannelDetails';
-export {default as SearchBar} from './SearchBar'
+export {default as SearchBar} from './SearchBar';
+export {default as SideBar} from './SideBar';
