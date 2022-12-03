87a973d91e45217c911d00d3899fbf0b9308329a
rumon5h <mdrumonkhan48519@gmail.com>
2022-12-03 11:31:30 +0600

    Fixed some issue

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -12,15 +12,12 @@ const Feed = () => {
     },[selectedCategory])
     return (
         <Stack sx={{flexDirection: {sx: 'column', md: 'row'}}}>
-            <Box sx={{height: {sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px:{sx:0, md: 2}}}>
+            <Box sx={{height: {sx: 'auto', md: 'fit-content'}, borderRight: '1px solid #3d3d3d', px:{sx:0, md: 2}}}>
 
             <SideBar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
 
-            <Typography className='copyRight' variant='body2' sx={{mt: 1.5, color: '#fff'}}>
-            CopyRight 2022 Rumon 
-            </Typography>
             </Box>
-            <Box p={2} sx={{overFlowY: 'auto', height: '90vh', flex: 2}}>
+            <Box p={2} sx={{overFlowY: 'auto', height: 'fit-content', flex: 2}}>
                 <Typography variant='h4' fontWeight="bold" mb={2} sx={{color: 'white'}}>
                     {selectedCategory} <span style={{color: '#F31503'}}>videos</span>
                 </Typography>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -20,7 +20,7 @@ const SearchBar = () => {
             onSubmit={handleSubmit}
             sx={{ borderRadius: 20, border: '1px solid #e3e3e3', pl: 2, boxShadow: 'none', mr: { sm: 5 } }}
         >
-            <input type="text" className='search-bar' placeholder='Search....' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+            <input type="text" className='search-bar' placeholder='Search...' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
             <IconButton type='submit' sx={{ p: '10px', color: 'red' }}>
                 <Search></Search>
             </IconButton>
